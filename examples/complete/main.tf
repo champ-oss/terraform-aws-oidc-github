c331efbce2817ce614619b8c960597688835fd57
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-09 12:04:35 -0500

    add initial release of module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,14 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+locals {
+  git = "terraform-aws-oidc"
+}
+
 module "this" {
   source = "../../"
+  trusted_repos = {
+    values : ["repo:my-org/my-repo:pull_request","org/repo"]
+  }
 }
