4f0212ac10aa9af3df50bf77bcf444d4d4c09ebb
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-15 09:26:21 -0500

    testing new read and admin roles

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "iam_role_arn" {
+output "iam_role_admin_arn" {
   description = "IAM roles created mapping for github"
-  value       = aws_iam_role.this.arn
+  value       = aws_iam_role.admin.arn
+}
+
+output "iam_role_read_arn" {
+  description = "IAM roles created mapping for github"
+  value       = aws_iam_role.read.arn
 }
