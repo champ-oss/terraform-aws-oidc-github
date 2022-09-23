1258d3eba01f37601a44ec608c790d4a13a7917f
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-23 16:02:39 -0500

    enable flag for provider

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "iam_role_read_arn" {
   description = "IAM roles created mapping for github"
   value       = aws_iam_role.read[0].arn
 }
+
+output "custom_iam_role_arn" {
+  description = "custom iam role"
+  value       = var.custom_policy != null ? aws_iam_role.custom[0].arn : null
+}
+
+output "custom_iam_role_name" {
+  description = "custom iam name"
+  value       = var.custom_policy != null ? aws_iam_role.custom[0].name : null
+}
