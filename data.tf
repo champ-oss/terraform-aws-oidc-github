5b44add9874b5f92ef5eb9959c662024a0a50ac8
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-26 12:50:56 -0500

    enhancements

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 locals {
   read_repos                      = var.trusted_read_repos != null ? var.trusted_read_repos : []
   admin_repos                     = var.trusted_admin_repos != null ? var.trusted_admin_repos : []
-  aws_iam_openid_connect_provider = var.enable_oidc_provider ? aws_iam_openid_connect_provider.this[0].arn : null
+  aws_iam_openid_connect_provider = var.enable_oidc_provider ? aws_iam_openid_connect_provider.this[0].arn : ""
 }
 
 data "aws_iam_policy_document" "read" {
