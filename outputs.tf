784e0ab3c00e61df3a895a8c83634fd803201e5d
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-26 09:44:46 -0500

    testing changes to oidc

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "iam_role_admin_arn" {
   description = "IAM roles created mapping for github"
-  value       = aws_iam_role.admin[0].arn
+  value       = var.enable_admin_role != false ? aws_iam_role.admin[0].arn : null
 }
 
 output "iam_role_read_arn" {
   description = "IAM roles created mapping for github"
-  value       = aws_iam_role.read[0].arn
+  value       = var.enable_read_role != false ? aws_iam_role.read[0].arn : null
 }
 
 output "custom_iam_role_arn" {
