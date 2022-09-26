5ba1414259e400090ff81058cd0bc227d464edcc
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-26 16:06:29 -0500

    test oidc with many roles

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,15 +21,27 @@ module "this" {
 }
 
 # deploy oidc provider only
-module "oidc_with_many_roles" {
+module "oidc_only" {
+  source            = "../../"
+  git               = local.git
+  url               = "https://token.actions.githubusercontenttestoidconly.com"
+  name              = "oidc-github-oidc"
+  enable_admin_role = false
+  enable_read_role  = false
+}
+
+# deploy oidc provider only
+module "github_oidc_roles" {
   for_each = toset([
     "terraform-aws-repo1",
     "terraform-aws-repo2",
   ])
-  source            = "../../"
-  git               = local.git
-  url               = "https://token.actions.githubusercontenttestoidconly.com"
-  name              = each.value
+  source               = "../../"
+  git                  = local.git
+  enable_oidc_provider = false
+  name                 = each.value
   trusted_read_repos   = concat(formatlist("repo:champtitles/%s:*", each.value), formatlist("repo:champtitles/%s:pull_request", each.value))
   trusted_admin_repos  = formatlist("repo:champtitles/%s:ref:refs/heads/main", each.value)
+  oidc_provider_arn    = module.oidc_only.oidc_provider_arn
+  depends_on           = [module.oidc_only]
 }
