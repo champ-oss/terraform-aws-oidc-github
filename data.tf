f9b4d29d0dc39959d086df0d57c6e247a7a91954
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-26 10:56:02 -0500

    testing changes to oidc

--- data.tf
+++ data.tf
@@ -4,10 +4,9 @@ locals {
 }
 
 data "aws_iam_policy_document" "read" {
-  count = var.enable_read_role ? 1 : 0
   statement {
     principals {
-      identifiers = var.openid_arn != null ? [var.openid_arn] : [aws_iam_openid_connect_provider.this[0].arn]
+      identifiers = var.enable_read_role != true ? [var.openid_arn] : [aws_iam_openid_connect_provider.this[0].arn]
       type        = "Federated"
     }
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -25,10 +24,9 @@ data "aws_iam_policy_document" "read" {
 }
 
 data "aws_iam_policy_document" "admin" {
-  count = var.enable_admin_role ? 1 : 0
   statement {
     principals {
-      identifiers = var.openid_arn != null ? [var.openid_arn] : [aws_iam_openid_connect_provider.this[0].arn]
+      identifiers = var.enable_admin_role != true ? [var.openid_arn] : [aws_iam_openid_connect_provider.this[0].arn]
       type        = "Federated"
     }
     actions = ["sts:AssumeRoleWithWebIdentity"]
